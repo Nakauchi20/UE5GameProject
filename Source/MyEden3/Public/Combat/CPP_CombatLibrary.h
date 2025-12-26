ae88b451487d4064e3df56e085594e53804b747d
Gepp00 <lireb.tacr0@gmail.com>
2025-12-26 17:19:07 +0900

    .vs folder removed from git

---
+++ Source/MyEden3/Public/Combat/CPP_CombatLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CPP_CombatLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYEDEN3_API UCPP_CombatLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/MyEden3/Public/Data/CPP_EnemyStatsData.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "CPP_EnemyStatsData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYEDEN3_API UCPP_EnemyStatsData : public UDataTable
+{
+	GENERATED_BODY()
+	
+};
